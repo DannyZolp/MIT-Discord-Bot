9e53599ae557e1a8f4f7014d9fc416d1c17aaebe
Danny Zolp <danny@zolp.dev>
2020-10-03 17:50:14 -0500

    Add code

---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag} !`);
+})
+
+client.on('message', async message => {
+    // create our regex
+    const regex = new RegExp(/(p)+\w+(is)/g)
+
+    // if our bot sent the message, don't do anything.
+    if (message.author.bot) return;
+
+    // creates our compressed message that makes getting around the filter harder
+    var compressedMessage = message.content;
+
+    // removes all of the spaces from the string
+    compressedMessage = compressedMessage.replace(/\s+/g, '')
+
+    // removes all of the special characters from the string
+    compressedMessage = compressedMessage.replace(/[^a-zA-Z ]/g, '')
+
+    // makes the entire string lowercase
+    compressedMessage = compressedMessage.toLowerCase();
+
+    // actually check the string
+    if (regex.test(compressedMessage)) {
+        await message.delete()
+    }
+})
+
+client.login(process.env.DISCORD_KEY)
