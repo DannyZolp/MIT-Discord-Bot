e723ba91cbcd2bf771fbf81bc5d6feb475fa690f
Danny Zolp <danny@zolp.dev>
2020-10-04 11:57:11 -0500

    Fixed bugs, added multi-user support

--- index.js
+++ index.js
@@ -1,79 +1,144 @@
-const Discord = require('discord.js');
-require('dotenv').config();
+const Discord = require("discord.js");
+require("dotenv").config();
 
 const client = new Discord.Client();
 
-var messageLog = new Array();
+var messageLogs = {};
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag} !`);
-})
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag} !`);
+});
 
-client.on('message', async message => {
-    // create our regex
-    const regex = new RegExp(/(p)+\w+[is]/g)
+client.on("message", async (message) => {
+  // create our regex
+  const regex = new RegExp(/(p)+\w+[ei]+s*/g);
 
-    // if our bot sent the message, don't do anything.
-    if (message.author.bot /*|| message.author.id !== "467861795676094475"*/) return;
+  // if our bot sent the message, don't do anything.
+  if (message.author.bot /*|| message.author.id !== "456099370799202304"*/)
+    return;
 
-    // creates our compressed message that makes getting around the filter harder
-    var compressedMessage = message.content;
+  // creates our compressed message that makes getting around the filter harder
+  var compressedMessage = await removeExtraBits(message.content);
 
-    // removes all of the spaces from the string
-    compressedMessage = compressedMessage.replace(/\s+/g, '')
+  // add our message to our log to ensure there is no multi-line penises
+  await addMessageToLog(message);
 
-    // removes all of the special characters from the string
-    compressedMessage = compressedMessage.replace(/[^a-zA-Z ]/g, '')
+  // check our messages for penises
+  if (regex.test(compressedMessage) || compressedMessage === "pen15") {
+    // if a singular message has penis
 
-    // makes the entire string lowercase
-    compressedMessage = compressedMessage.toLowerCase();
+    console.log("Deleting!");
+    await message.delete();
+    messageLogs[message.author.id] = new Array();
+  } else if (
+    regex.test(
+      await removeExtraBits(
+        await turnArrayToString(messageLogs[message.author.id])
+      )
+    )
+  ) {
+    // if a multi-line penis
 
-    // add our message to our log to ensure there is no multi-line penises
-    await addMessageToLog(message);
+    console.log("Deleting Multi-Line!");
+    messageLogs[message.author.id] = await deleteAllMessages(messageLogs[message.author.id]);
+  }
+});
 
-    // actually check the string
-    if (regex.test(compressedMessage) || compressedMessage === "pen15") {
-        console.log("Deleting!")
-        await message.delete()
-    } else if (regex.test( await turnMessageLogToString() )) {
-        console.log("Deleting Multi-Line!")
-        await deleteAllMessagesInLog();
-    }
-})
-
+/**
+ *
+ * @param {Discord.Message} message
+ */
 async function addMessageToLog(message) {
-    if (messageLog.length >= 5) {
-        messageLog = await addAndRemoveOverflow(messageLog, message)
-    } else {
-        messageLog.push(message)
+  // Grabs the array we want to work with, or creates a new array for the user.
+  var messageLog =
+    messageLogs[message.author.id] === undefined
+      ? new Array()
+      : messageLogs[message.author.id];
+
+  if (messageLog.length >= 5) {
+    // makes sure our array isn't storing more than 5 things, and then deletes overflow
+    messageLog = await addAndRemoveOverflow(messageLog, message);
+  } else {
+    // justs adds the message to the array
+    messageLog.push(message);
+  }
+
+  // adds our modified array back to the object
+  messageLogs[message.author.id] = messageLog;
+
+  // returns to complete the promise
+  return;
+}
+
+/**
+ *
+ * @param {Array<Discord.Message>} array
+ */
+async function turnArrayToString(array) {
+  // creates a string for concatination
+  var string = "";
+
+  // goes through our array
+  array.forEach((message) => {
+    // adds each of the message's content to our string
+    string += message.content;
+  });
+
+  // returns the string
+  return string;
+}
+
+/**
+ *
+ * @param {Array<Discord.Message>} array
+ */
+async function deleteAllMessages(array) {
+  // goes through our array
+  array.forEach(async (message) => {
+    try {
+      // deletes all of the messages
+      await message.delete();
+    } catch (e) {
+      console.log(e);
     }
+  });
 
-    return;
+  // returns a new array
+  return new Array();
 }
 
-async function turnMessageLogToString() {
-    var string = "";
-    messageLog.forEach(message => {
-        string += message.content;
-    })
-    return string;
-}
-
-async function deleteAllMessagesInLog() {
-    messageLog.forEach(async message => {
-        await message.delete();
-    })
-    messageLog = new Array();
-    return;
-}
-
+/**
+ *
+ * @param {Array} array
+ * @param {*} newElement
+ */
 async function addAndRemoveOverflow(array, newElement) {
-    array.push(newElement)
-    array.splice(0, 1);
+  // adds the new element to the array
+  array.push(newElement);
 
-    if (array >= 5) array = new Array();
+  // remvoes the first element from the array
+  array.splice(0, 1);
 
-    return array;
+  // returns our modified array
+  return array;
 }
 
-client.login(process.env.DISCORD_KEY)
+/**
+ *
+ * @param {String} string
+ */
+async function removeExtraBits(string) {
+  // removes all of the spaces from the string
+  string = string.replace(/\s+/g, "");
+
+  // removes all of the special characters from the string
+  string = string.replace(/[^a-zA-Z ]/g, "");
+
+  // makes the entire string lowercase
+  string = string.toLowerCase();
+
+  return string;
+}
+
+// Login to Discord
+client.login(process.env.DISCORD_KEY);
