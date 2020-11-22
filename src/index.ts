11ffc89b97ca26ea5ecb2a979ad28eb7cd41c8e1
Danny Zolp <dannydog5186@gmail.com>
2020-11-22 13:01:30 -0600

    Just have the health check run by default because happiness is not allowed

--- src/index.ts
+++ src/index.ts
@@ -7,38 +7,37 @@ const http = require('http');
 const bot = new Client();
 
 bot.on('ready', () => {
-    console.log(`Connected to API as ${bot.user.tag}`)
-    HealthCheckSatisfier();
-})
+    console.log(`Connected to API as ${bot.user.tag}`);
+});
 
 bot.on('message', (message: Message) => {
-    retext().use(spell, dictionary).process(message.content, (err, file) => {
-        if (message.content.includes('penis') || message.content.includes('pen15')) {
-            message.delete();
-            return;
-        } 
-        if (file.messages !== []) {
-            file.messages.forEach(element => {
-                element.expected.forEach(expected => {
-                    if (expected === 'penis') {
-                        message.delete()
-                    }
-                })
-            });
-        }
-    })
-})
+    retext()
+        .use(spell, dictionary)
+        .process(message.content, (err, file) => {
+            if (message.content.includes('penis') || message.content.includes('pen15')) {
+                message.delete();
+                return;
+            }
+            if (file.messages !== []) {
+                file.messages.forEach((element) => {
+                    element.expected.forEach((expected) => {
+                        if (expected === 'penis') {
+                            message.delete();
+                        }
+                    });
+                });
+            }
+        });
+});
 
-bot.login(process.env.BOT_TOKEN)
+bot.login(process.env.BOT_TOKEN);
 
-function HealthCheckSatisfier() {
-    const server = http.createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(`Connected to API as ${bot.user.tag}`);
-      });
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`Connected to API as ${bot.user.tag}`);
+});
 
-      server.listen(process.env.PORT, '127.0.0.1', () => {
-          console.log('Health check successfully initialized')
-      })
-}
+server.listen(process.env.PORT, '127.0.0.1', () => {
+    console.log('Health check successfully initialized');
+});
